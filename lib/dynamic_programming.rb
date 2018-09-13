08ba78ef305eb85fb4a0faea4e0ac592c1fb0714
Jordan Yu <jordanyu44@gmail.com>
2018-09-13 00:09:04 -0700

    Build blair nums bottom up

--- lib/dynamic_programming.rb
+++ lib/dynamic_programming.rb
@@ -2,16 +2,31 @@ class DynamicProgramming
   attr_accessor :blair_cache
 
   def initialize
-    @blair_cache = {}
+    @blair_cache = {1 => 1, 2 => 2}
   end
 
   def blair_nums(n)
     return blair_cache[n] if blair_cache[n]
-    return n if n == 1 || n == 2
     k_odd = (n - 2) * 2 + 1 
     ans = blair_nums(n - 1) + blair_nums(n - 2) + k_odd
     self.blair_cache[n] = ans
     return ans
+  end
+
+  def blair_nums_cache_builder(n)
+    cache = { 1 => 1, 2 => 2 }
+    return cache if n < 3
+    3.upto(n) do |num|
+      k_odd = (num - 2) * 2 + 1 
+      ans = cache[num - 1] + cache[num - 2] + k_odd
+      cache[num] = ans
+    end
+    cache
+  end
+
+  def blair_nums_bottom_up(n)
+    cache = blair_nums_cache_builder(n)
+    cache[n]
   end
 
   def frog_hops_bottom_up(n)
